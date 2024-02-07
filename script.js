25c6f10374a0a7c0e9618c69fb83abce138511d2
Roman Stix <rsonroad@gmail.com>
2024-02-07 22:53:41 +0100

    addinng done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log(1)
+document.getElementById('addRowBtn').addEventListener('click', function() {
+  // Create a new table row
+  var newRow = document.createElement('div');
+  newRow.classList.add('table-row');
+
+  // Create input fields for each column
+  for (var i = 0; i < 3; i++) {
+    var newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
+    newInput.classList.add('table-row-item');
+    newRow.appendChild(newInput);
+  }
+
+  for (var i = 0; i < 2; i++) {
+    var newInput = document.createElement('div');
+    newInput.classList.add('table-row-item');
+    newRow.appendChild(newInput);
+  }
+
+  // Append the new row to the table
+  document.querySelector('.table').appendChild(newRow);
+});
